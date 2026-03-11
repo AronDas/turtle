78ff9a8fe51cbba015663666c91fc5902b7ab362
AronDas <arondas.sg@gmail.com>
2026-03-11 19:42:49 -0300

    Add files via upload

---
+++ drawastar.py
@@ -0,0 +1,35 @@
+import turtle
+
+turtle.Screen().bgcolor("purple")
+
+turtle.Screen().setup(400,500)
+
+tri = turtle.Turtle()
+
+tri.forward(100)
+
+tri.right(120)
+
+tri.forward(100)
+
+tri.right(120)
+
+tri.forward(100)
+
+tri.left(150)
+
+tri.penup()
+
+tri.forward(50)
+
+tri.left(90)
+
+tri.pendown()
+
+tri.forward(100)
+tri.left(120)
+tri.forward(100)
+tri.left(120)
+tri.forward(100)
+
+turtle.done()
