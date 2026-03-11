b6dba2ccac4e03d2f3e7d441efafe51ad52de9c3
AronDas <arondas.sg@gmail.com>
2026-03-11 19:47:47 -0300

    Add files via upload

---
+++ drawasquare.py
@@ -0,0 +1,19 @@
+import turtle
+turtle.Screen().bgcolor("purple")
+turtle.Screen().setup(600,700)
+
+squ = turtle.Turtle()
+
+squ.forward(100)
+squ.right(90)
+
+squ.forward(100)
+squ.right(90)
+
+squ.forward(100)
+squ.right(90)
+
+squ.forward(100)
+squ.right(90)
+
+turtle.done()
